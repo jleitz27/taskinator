8d7dd0b6cc2b66e747a182ee6a9f1abf7352f7fc
Jason L <jkleitz27@gmail.com>
2022-04-19 18:21:07 -0400

    add ability to submit custom tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,15 @@ var taskDataObj = {
     type: taskTypeInput
     };
 
-  // send it as an argument to createTaskEl
+    // check if input values are empty strings
+    if(!taskNameInput || !taskTypeInput){
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    formEl.reset();
+
+    // send it as an argument to createTaskEl
     createTaskEl(taskDataObj);
 };
 
